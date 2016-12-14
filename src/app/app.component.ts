4b5e315cf01aec9f852b8d03f1ff99868cdf8474
Patrick Werner <pwerner2@gmail.com>
2016-12-14 09:54:02 -0500

    created input, button click working. Angular 2 is cool

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome to GiphySearch!';
+  performSearch(): void {
+    console.log("Button click is working!");
+  }
 }
